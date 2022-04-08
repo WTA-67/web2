078edcc89bda9a5282da45df2fe3b62fd4956e82
Weam Atawy <wmat6885@gmail.com>
2022-04-08 21:25:41 +0300

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const mysql=require("mysql");
 const app =express();
+const dotenv=require('dotenv');
+dotenv.config({path:'./.env'});
+const path=require('path');
 const db=mysql.createConnection({
-   host:'localhost',
-   user:'root',
-   password:'',
-   database:'nod js-login'
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE
 });
-
+const publicDirectory= path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.set('view engine','hbs');
 db.connect((error)=>{
     if(error){
         console.log(error)
@@ -16,4 +23,8 @@ db.connect((error)=>{
         console.log("MySql connected")
     }
 });
-
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+app.listen(5000,()=>{
+    console.log("Server started on port 5000");
+});
